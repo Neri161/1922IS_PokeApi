006e1305c4a2d11ad58e898a33cfb67bb31075b3
Neri161 <Neri010601>
2022-06-17 00:14:23 -0500

    migracion usuario

---
+++ ApiMagic/database/migrations/2022_06_17_051034_usuario.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Usuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido_Paterno');
+            $table->string('apellido_Materno');
+            $table->string('correo');
+            $table->string('contraseña');
+            $table->dateTime('fecha_Nacimiento');
+            $table->boolean('estatus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
