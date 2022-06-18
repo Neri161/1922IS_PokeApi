64fbc66018cc23ac9a3c2d31433a8cb038934f60
Fabian0121 <Fabian0121>
2022-06-17 23:11:09 -0500

    Registro

--- ApiMagic/app/Http/Controllers/UsuarioController.php
+++ ApiMagic/app/Http/Controllers/UsuarioController.php
@@ -21,4 +21,38 @@ class UsuarioController extends Controller
         return $usuario;
 
     }
+
+    public function registrarUsuario(Request $datos){
+
+        if (!$datos->nombre || !$datos->apellido_Paterno || !$datos->apellido_Materno || !$datos->correo || !$datos->fecha_Nacimiento || !$datos->contrasenia || !$datos->contrasenia2)
+            return ["estatus" => "Error", "Mensaje" => "Algunos campos estan vacios"];
+
+        //Se verifica si existe una cuenta ya
+        $usuario = Usuario::where('correo', $datos->correo)->first();
+        if ($usuario)
+            return ["estatus" => "Error", "Mensaje" => "El correo ya esta registrado"];
+        //Se comparan las contrasenias
+        if ($datos->contrasenia != $datos->contrasenia2)
+            return ["estatus" => "Error", "Mensaje" => "Las contrasenias no son iguales"];
+
+        $nombre = $datos->nombre;
+        $apellidoP = $datos->apellido_Paterno;
+        $apellidoM = $datos->apellido_Materno;
+        $correo = $datos->correo;
+        $contrasenia = password_hash($datos->contrasenia, PASSWORD_DEFAULT);
+        $fechaNacimiento = $datos->fecha_Nacimiento;
+        $estatus = "";
+
+        $usuario = new Usuario();
+        $usuario->nombre = $nombre;
+        $usuario->apellido_Paterno = $apellidoP;
+        $usuario->apellido_Materno = $apellidoM;
+        $usuario->correo = $correo;
+        $usuario->contrasenia = $contrasenia;
+        $usuario->fecha_Nacimiento = $fechaNacimiento;
+        $usuario->estatus =$estatus;
+
+        $usuario->save();
+        return ["Mensaje" => "Cuenta creada"];
+    }
 }
