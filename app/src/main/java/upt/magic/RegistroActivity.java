d8ba2e2c8a04c4b6291f7f1959b249546b19081b
Macover <macover1507@hotmail.com>
2022-06-18 18:36:17 -0500

    Vista registro e iconos agregados

--- app/src/main/java/upt/magic/RegistroActivity.java
+++ app/src/main/java/upt/magic/RegistroActivity.java
@@ -3,12 +3,17 @@ package upt.magic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistroActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+    public void muestraToas(View view){
+        Toast.makeText(this, "Le diste click!", Toast.LENGTH_SHORT).show();
     }
 }
