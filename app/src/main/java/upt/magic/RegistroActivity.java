bbafd3f554a6124cb4a08f612f199681a98c297c
Macover <macover1507@hotmail.com>
2022-06-18 19:44:14 -0500

    Funcionalidad datePicker a input fechaNacimiento

--- app/src/main/java/upt/magic/RegistroActivity.java
+++ app/src/main/java/upt/magic/RegistroActivity.java
@@ -2,18 +2,62 @@ package upt.magic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class RegistroActivity extends AppCompatActivity {
+
+    DatePickerDialog picker;
+    EditText fechaNacimiento;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_registro);
+
+    fechaNacimiento = (EditText) findViewById(R.id.in_fechaNacimieto);
+
+    /*findViewById(R.id.in_fechaNacimieto).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            final Calendar cldr = Calendar.getInstance();
+            int day = cldr.get(Calendar.DAY_OF_MONTH);
+            int month = cldr.get(Calendar.MONTH);
+            int year = cldr.get(Calendar.YEAR);
+            // date picker dialog
+            picker = new DatePickerDialog(RegistroActivity.this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            fechaNacimiento.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        }
+                    }, year, month, day);
+            picker.show();
+        }
+    });*/
     }
+
     public void muestraToas(View view){
         Toast.makeText(this, "Le diste click!", Toast.LENGTH_SHORT).show();
     }
+    public void onClickFechaNacimiento(View v) {
+        final Calendar cldr = Calendar.getInstance();
+        int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+        // date picker dialog
+        picker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        fechaNacimiento.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+        picker.show();
+    }
+
 }
