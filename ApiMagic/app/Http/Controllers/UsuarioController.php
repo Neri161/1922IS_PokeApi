45edaa92a0dd3a83adcd2d655a5c8dcc9315f510
Fabian0121 <fabianjuarezsandoval@gmail.com>
2022-06-21 21:28:22 -0500

    Validaciones de registro agregados

--- ApiMagic/app/Http/Controllers/UsuarioController.php
+++ ApiMagic/app/Http/Controllers/UsuarioController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Usuario;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UsuarioController extends Controller
 {
@@ -24,35 +26,46 @@ class UsuarioController extends Controller
 
     public function registrarUsuario(Request $datos){
 
-        if (!$datos->nombre || !$datos->apellido_Paterno || !$datos->apellido_Materno || !$datos->correo || !$datos->fecha_Nacimiento || !$datos->contrasenia || !$datos->contrasenia2)
-            return ["estatus" => "Error", "Mensaje" => "Algunos campos estan vacios"];
+        $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $rules = [
+            'nombre' => "required|min:3|max:32|alpha",
+            'apellido_Paterno' => "required|min:3|max:32|alpha",
+            'apellido_Materno' => "required|min:3|max:32|alpha",
+            'correo' => "required|email|min:8|max:64",
+            'fecha_Nacimiento' => "required|date",
+            'contrasenia' => "required|min:8|max:64",
+            'contrasenia2' => "required|min:8|max:64"
+        ];
+        try {
+            //Validacion
+            $validator = Validator::make($datos->all(), $rules);
+            if ($validator->fails()){
+                return $validator->errors();
+            }else{
+                //Se verifica si existe una cuenta ya
+                $usuario = Usuario::where('correo', $datos->correo)->first();
+                if ($usuario)
+                    return ["estatus" => "Error", "mensaje" => "El correo ya esta registrado"];
+                //Se comparan las contrasenias
+                if ($datos->contrasenia != $datos->contrasenia2)
+                    return ["estatus" => "Error", "mensaje" => "Las contrasenias no son iguales"];
 
-        //Se verifica si existe una cuenta ya
-        $usuario = Usuario::where('correo', $datos->correo)->first();
-        if ($usuario)
-            return ["estatus" => "Error", "Mensaje" => "El correo ya esta registrado"];
-        //Se comparan las contrasenias
-        if ($datos->contrasenia != $datos->contrasenia2)
-            return ["estatus" => "Error", "Mensaje" => "Las contrasenias no son iguales"];
-
-        $nombre = $datos->nombre;
-        $apellidoP = $datos->apellido_Paterno;
-        $apellidoM = $datos->apellido_Materno;
-        $correo = $datos->correo;
-        $contrasenia = password_hash($datos->contrasenia, PASSWORD_DEFAULT, ['cost' => 5]);
-        $fechaNacimiento = $datos->fecha_Nacimiento;
-        $estatus = 1;
-
-        $usuario = new Usuario();
-        $usuario->nombre = $nombre;
-        $usuario->apellido_Paterno = $apellidoP;
-        $usuario->apellido_Materno = $apellidoM;
-        $usuario->correo = $correo;
-        $usuario->contrasenia = $contrasenia;
-        $usuario->fecha_Nacimiento = $fechaNacimiento;
-        $usuario->estatus =$estatus;
-
-        $usuario->save();
-        return ["Mensaje" => "Cuenta creada"];
+                $codigo_confirmacion = substr(str_shuffle($caracteres),0, 64);
+                //Se registran los datos
+                $usuario = new Usuario();
+                $usuario->nombre = $datos->nombre;
+                $usuario->apellido_Paterno = $datos->apellido_Paterno;
+                $usuario->apellido_Materno = $datos->apellido_Materno;
+                $usuario->correo = $datos->correo;
+                $usuario->contrasenia = password_hash($datos->contrasenia, PASSWORD_DEFAULT, ['cost' => 5]);
+                $usuario->fecha_Nacimiento = $datos->fecha_Nacimiento;
+                $usuario->estatus = 1;
+                $usuario->codigo_confirmacion = $codigo_confirmacion;
+                $usuario->save();
+                return ["mensaje" => "Cuenta creada"];
+            }
+        }catch (Exception $e){
+            return (['estatus' => "Error", 'mensaje' => "Algo salio mal intenta de nuevo"]);
+        }
     }
 }
