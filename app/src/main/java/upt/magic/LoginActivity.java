d81821824d07330c4f7a8141d68a38a780ec7093
Neri161 <Neri010601>
2022-06-29 22:29:01 -0500

    recuperar

--- app/src/main/java/upt/magic/LoginActivity.java
+++ app/src/main/java/upt/magic/LoginActivity.java
@@ -2,6 +2,7 @@ package upt.magic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -91,5 +92,13 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    public void registro(View view){
+        Intent i = new Intent(LoginActivity.this,RegistroActivity.class);
+        startActivity(i);
+    }
+    public void restablecer(View view){
+        Intent i = new Intent(LoginActivity.this,RestablecerActivity.class);
+        startActivity(i);
     }
 }
