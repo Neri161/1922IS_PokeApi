2ce85e944f34381b1f5182d6b8b034711efe2c94
Alfred-in <alfredo_1319104653@uptecamac.edu.mx>
2022-06-29 21:20:43 -0500

    Post Registrar v2

--- app/src/main/java/upt/magic/registroApi.java
+++ app/src/main/java/upt/magic/registroApi.java
@@ -12,26 +12,34 @@ public class registroApi {
     public String error;
     public static String URL = "https://viqoxwhm.lucusvirtual.es/public/api/";
     public static Retrofit varRetro;
+
     public void registrar(Usuario usuario) {
+        varRetro = new Retrofit.Builder().baseUrl(URL).addConverterFactory(GsonConverterFactory.create()).build();
         PeticionUsuario consApi = varRetro.create(PeticionUsuario.class);
-        Call<Usuario> call = consApi.registrar(usuario.getNombre(), usuario.getApellidoPaterno(), usuario.getApellidoMaterno(), usuario.getCorreo(), usuario.getContrasenia(), usuario.getContrasenia2(), usuario.getFechaNacimiento());
-            varRetro = new Retrofit.Builder().baseUrl(URL)
-                    .addConverterFactory(GsonConverterFactory.create()).build();
+        Usuario usu=new Usuario("Alfred","Lopez","Bautista","sorrow2287@gmail.com","Alfredo1234$","Alfredo1234$","2001-08-13");
+        Call<Usuario> call = consApi.registrar(usu);
             call.enqueue(new Callback<Usuario>() {
                 @Override
                 public void onResponse(Call<Usuario> call, Response<Usuario> response) {
-                if(!response.isSuccessful()){
-                    error="No fue exitoso";
-                    return;
-                }
-                Usuario usu=response.body();
+                    try {
+                        if (response.isSuccessful()) {
+                            Usuario m = response.body();
+                            System.out.println(call);
+                            System.out.println(m.getNombre());
+                            System.out.println(m.getCorreo());
+
+                        }
+                    } catch (Exception ex) {
+                        System.out.println("UWUn't " + ex);
+                    }
+               /* Usuario usu=response.body();
                 String content="";
                 content+="Nombre: "+usuario.getNombre()+"\n";
                 content+="Apellido Paterno: "+usuario.getApellidoPaterno()+"\n";
                 content+="Apellido Materno: "+usuario.getApellidoMaterno()+"\n";
                 content+="Correo: "+usuario.getCorreo()+"\n";
                 content+="Contraseña: "+usuario.getContrasenia()+"\n";
-                content+="Fecha: nacimiento"+usuario.getFechaNacimiento()+"\n";
+                content+="Fecha: nacimiento"+usuario.getFechaNacimiento()+"\n";*/
                 }
 
                 @Override
